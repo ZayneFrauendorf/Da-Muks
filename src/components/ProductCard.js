d4d8193f5db5d1f9aaea680613375fac9a17dd99
Zayne <zaynef@outlook.com>
2018-05-11 13:40:52 +1200

    first-build

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,18 +4,28 @@ class ProductCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      added: false
+      added: false,
+      selectedProduct: null
     };
-
-    // this.toggleAdded = this.toggleAdded.bind(this);
   }
+
+  setWeight = ({ target: { value } }) => {
+    const found = this.props.productDetails.find(obj => obj.weight === value);
+    this.setState({
+      weight: value,
+      selectedProduct: found
+    });
+  };
+
   toggleAdded = () => {
     this.setState({
       added: !this.state.added
     });
   };
   render() {
-    const { img, title, description, price } = this.props;
+    const { img, title, description, productDetails } = this.props;
+    console.log(productDetails);
+    const { weight, added, selectedProduct } = this.state;
     return (
       <React.Fragment>
         <div className="product">
@@ -27,32 +37,46 @@ class ProductCard extends Component {
             <div className="wording">
               <p>{description}</p>
             </div>
-            {this.state.added === false ? (
+            {added === false ? (
               <div className="action-buttons-ctr">
                 <button
                   className="snipcart-add-item"
-                  data-item-id="2"
+                  data-item-id={selectedProduct && selectedProduct.weight === "250G" ? "1" : "5"}
                   data-item-name="Raw Honey"
-                  data-item-price="45.00"
-                  data-item-weight="20"
+                  data-item-price={selectedProduct && selectedProduct.price}
+                  data-item-weight={selectedProduct && selectedProduct.weight}
                   data-item-url="http://myapp.com/products/bacon"
                   data-item-description="Raw honey"
                   onClick={() => this.toggleAdded()}
                 >
                   Add to Cart
                 </button>
+                <select value={weight} onChange={this.setWeight}>
+                  <option value="" selected disabled hidden>
+                    Select Weight
+                  </option>
+                  {productDetails.map(productDetail => {
+                    return (
+                      <option value={productDetail.weight}>
+                        {productDetail.weight}
+                      </option>
+                    );
+                  })}
+                </select>
               </div>
             ) : (
               <React.Fragment>
-              <div className="action-buttons-ctr">
-              <span>Item Added!</span>
-                <button className="snipcart-checkout">Checkout</button>
-                <button  onClick={() => this.toggleAdded()}>Continue Shopping</button>
-              </div>
+                <div className="action-buttons-ctr">
+                  <span>Item Added!</span>
+                  <button className="snipcart-checkout">Checkout</button>
+                  <button onClick={() => this.toggleAdded()}>
+                    Continue Shopping
+                  </button>
+                </div>
               </React.Fragment>
             )}
           </div>
-          <span className="price">{price}</span>
+          <span className="price">${selectedProduct && selectedProduct.price}</span>
         </div>
       </React.Fragment>
     );
